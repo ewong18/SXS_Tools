8645bb85d1c57b6a7e3699f8ba0c195e29805c88
Evan Wong <ewong1816@gmail.com>
2026-07-16 19:07:49 -0400

    making it timezone agnostic

--- exp_calculator/src/calc_xp.py
+++ exp_calculator/src/calc_xp.py
@@ -1,5 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
+import pytz
 import sys
 
 def parse_xp_file():
@@ -17,7 +18,9 @@ def parse_xp_file():
 def calc_eta(current_level,
              current_exp,
              target_level,
-             XP_per_hr):
+             XP_per_hr,
+             current_ts,
+             tgt_timezone="America/New_York"):
     XP_required = 0
     cleaned_data = parse_xp_file()
 
@@ -26,8 +29,9 @@ def calc_eta(current_level,
     
     remaining_XP_required = XP_required - current_exp
     
+    # include daily 2hr speedup
     time_required_hr = remaining_XP_required/XP_per_hr
-    free_resets = count_9ams_passed(current_ts, time_required_hr*3600)
+    free_resets = count_resets_passed(current_ts, time_required_hr*3600)
     if free_resets > 0:
         #print(f"Will cross {free_resets} reset days.")
         remaining_time = time_required_hr - (2 * free_resets)
@@ -35,20 +39,26 @@ def calc_eta(current_level,
         remaining_time = time_required_hr
         
     eta = current_ts + timedelta(seconds=remaining_time*3600)
-    return(eta)
+    eta_converted = eta.astimezone(pytz.timezone(tgt_timezone))
+    return(eta_converted)
 
-def count_9ams_passed(start_time: datetime, delta_seconds: float) -> int:
+def count_resets_passed(start_time: datetime, delta_seconds: float) -> int:
+    reset_time = 13  # Reset time in UTC (13:00 UTC)
     end_time = start_time + timedelta(seconds=delta_seconds)
     
-    start_shifted = start_time - timedelta(hours=9)
-    end_shifted = end_time - timedelta(hours=9)
+    start_shifted = start_time - timedelta(hours=reset_time)
+    end_shifted = end_time - timedelta(hours=reset_time)
     
     days_passed = (end_shifted.date() - start_shifted.date()).days
     
     return days_passed
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
+    if len(sys.argv) == 5:
+        tgt_timezone = "America/New_York"
+    elif len(sys.argv) == 6:
+        tgt_timezone = sys.argv[5]
+    else:
         #print("Usage: python calc_xp.py <current_level> <current_exp> <target_level> <XP_per_hr>")
         sys.exit(1)
     
@@ -57,6 +67,12 @@ if __name__ == "__main__":
     target_level = int(sys.argv[3])
     XP_per_hr = float(sys.argv[4])
     
-    current_ts = datetime.now()
-    eta = calc_eta(current_level, current_exp, target_level, XP_per_hr)
+    current_ts = datetime.now(timezone.utc)
+    
+    eta = calc_eta(current_level,
+                   current_exp,
+                   target_level,
+                   XP_per_hr,
+                   current_ts,
+                   tgt_timezone=tgt_timezone)
     print(eta)
